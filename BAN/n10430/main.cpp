f85361e40faace170ded7abc5d838df94ef60066
cheezzjazz <vban@naver.com>
2021-03-03 21:17:31 +0900

    n10430 updated

---
+++ BAN/n10430/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  n10430
+//
+//  Created by Ban on 2021/02/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
