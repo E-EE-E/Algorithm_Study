9dde1f1f9d74ecf5676961e293d839993c66ea3c
cheezzjazz <vban@naver.com>
2021-03-18 20:02:26 +0900

    n15650 solved another way

--- BAN/n15650/main.cpp
+++ BAN/n15650/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-// 풀이 1 - 순서
-// 위치(index)에 start~n 중 하나 선택
-// 오름차순이므로 선택한 수(i) 다음 위치(index+1)에는 i+1~n
+// 풀이 2 - 선택
+// 오름차순으로 i를 키우며 선택할지 안할지로 수열만듬
 
 using namespace std;
 
 int a[10];
-void go(int index, int start, int n, int m) {
-    if(index == m) {
+void go(int i, int selected, int n, int m) {
+    if(selected == m) {
         for(int i = 0; i < m; ++i) {
             cout<<a[i];
             if(i != m-1) cout<<' ';
@@ -15,15 +14,16 @@ void go(int index, int start, int n, int m) {
         cout<<'\n';
         return;
     }
-    for(int i = start; i <= n; ++i) {
-        a[index] = i;
-        go(index+1, i+1, n, m);
-    }
+    if(i>n) return;
+    a[selected] = i;
+    go(i+1, selected+1, n, m);
+    a[selected] = 0;
+    go(i+1, selected, n, m);
 }
 int main(int argc, const char * argv[]) {
     int n, m;
     cin >>n>>m;
-    go(0, 1, n, m);
+    go(1, 0, n, m);
     
     return 0;
 }
