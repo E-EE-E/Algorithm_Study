2c22ceac79dcf9d7a64516dce0fc80425147c393
cheezzjazz <vban@naver.com>
2021-04-20 22:14:08 +0900

    n16947-dfs,bfs solved 2 updated

--- BAN/n16947/main.cpp
+++ BAN/n16947/main.cpp
@@ -1,63 +1,57 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
-#include <deque>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 vector<vector<int>> a(3000, vector<int>(3000));
 //순환선 찾기 (DFS)
-bool dfs(int x, int px, deque<int> &circle, vector<bool> &check, int start) {
-    if(check[x]) {
-        if(x == start) return true;
-        else return false;
-    }
-    circle.push_back(x);
-    check[x] = true;
+int dfs(int x, int px, vector<int> &check) {
+    if(check[x]==1) return x; //사이클 찾음
+    check[x] = 1; //방문
     for(int i = 0; i < a[x].size(); ++i) {
         int y = a[x][i];
         if(y != px) {
-            if(dfs(y, x, circle, check, start))
-                return true;
-            else {
-                if(!circle.empty())
-                    circle.pop_back();
+            int res = dfs(y, x, check);
+            if(res == -2) return -2; //사이클 찾았지만, x는 사이클 밖
+            if(res >= 0) {
+                check[x] = 2; //x는 사이클에 포함
+                if(x == res) return -2; //사이클 시작점 = 끝. 다음부터 사이클에 포함안됨
+                else
+                    return res; //사이클의 시작점 반환 = 사이클 중
             }
         }
     }
-    return false;
-}
-//순환선과의 최단거리 찾기 (DFS)
-void go(int x, int ans, int &min, vector<int> &check, deque<int> &circle) {
-    if(check[x] != -1) return;
-    check[x] = ans;
-    for(int k = 0; k < circle.size(); ++k) {
-        if(circle[k] == x) {
-            min = ans < min? ans:min;
-            return;
-        }
-    }
-    for(int i = 0; i < a[x].size(); ++i) {
-        go(a[x][i], ans+1, min, check, circle);
-    }
-    
+    return -1; //사이클 못찾음
 }
 void solution(int n) {
-    deque<int> circle;
+    vector<int> check(n, 0);
+    dfs(0, -1, check);
+    
+    //순환선과의 최단거리 찾기 (BFS)
+    queue<int> q;
+    vector<int> dist(n);
     for(int i = 0; i < n; ++i) {
-         vector<bool> check(n, false);
-        if(dfs(i, -1, circle, check, i) == false)
-            circle.clear();
-        else
-            break;
+        if(check[i] == 2) { //사이클
+            dist[i] = 0;
+            q.push(i);
+        } else{
+            dist[i] = -1;
+        }
     }
-//    for(int i = 0; i < circle.size(); ++i)
-//        cout<<circle[i]<<' ';
+    while(!q.empty()) {
+        int x = q.front(); q.pop();
+        for(int i = 0; i < a[x].size(); ++i) {
+            int y = a[x][i];
+            if(dist[y] == -1) {
+                dist[y] = dist[x] + 1;
+                q.push(y);
+            }
+        }
+    }
     for(int i = 0; i < n; ++i) {
-        vector<int> check(n, -1);
-        int min = 4000;
-        go(i, 0, min, check, circle);
-        cout<<min<<' ';
+        cout<<dist[i]<<' ';
     }
 }
 
