7e086c374294889802a193022b38543a2f152915
cheezzjazz <vban@naver.com>
2021-04-26 19:32:47 +0900

    n2003 solved 2 updated

--- BAN/n2003/main.cpp
+++ BAN/n2003/main.cpp
@@ -1,45 +1,51 @@
 /*
- 풀이 1
- 중복연산 과정 없애기
- i = a, j = b 이면 sum = A[a] + A[a+1] + ... + A[b]
- i = a, j = b+1 이면 sum = A[a] + A[a+1] + ... + A[b] + A[b+1]
- 파란 부분이 중복되는 부분이므로 A[b+1]만 이어서 더해주면 됨
+ 풀이  2 - 투포인터 (two pointer) 사용
 
- 시간 초과 안남! - 성공 (시간 : 40ms)
- 시간복잡도 : O(N^2)
+ 가장 효율적 - 성공 (시간 : 4ms)
+ 시간복잡도 : O(N)
  */
 #include <cstdio>
 #include <iostream>
 #include <vector>
 using namespace std;
 int solution(vector<int> &a, int n, int m) {
+    int left, right;
+    left = right = 0;
     int ans = 0;
-    for(int i = 0; i < n; ++i) {
-        int sum = 0;
-        for(int j = i; j < n; ++j) {
-            sum += a[j];
-            if(sum == m) ans += 1;
+    int sum = a[0]; // 주의 (두 점이 같은 곳을 가리킴)
+    while(left <= right && right <n) {
+        if(sum < m) {
+            right += 1;
+            sum += a[right];
+        } else if(sum == m) {
+            ans += 1;
+            right += 1;
+            sum += a[right];
+        } else if(sum > m) {
+            sum -= a[left];
+            left += 1;
+            if(left > right && left < n) {
+                right = left;
+                sum = a[left];
+            }
         }
     }
-    
     return ans;
 }
-int main(int argc, const char * argv[]) {
+int main() {
     freopen("input.txt", "r", stdin);
     int tc;
-    cin >> tc;
+    cin>> tc;
     for(int t = 1; t <= tc; ++t) {
         int n, m;
         cin>>n>>m;
         vector<int> a;
         for(int i = 0; i < n; ++i) {
-            int x;
-            cin>>x;
+            int x; cin>>x;
             a.push_back(x);
         }
         cout<<'#'<<t<<' ';
-        cout<<solution(a,n, m);
-        cout<<'\n';
+        cout<<solution(a, n, m)<<'\n';
     }
     return 0;
 }
