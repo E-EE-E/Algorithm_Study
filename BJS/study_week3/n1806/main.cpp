b0cbbdd8cbe00e467b2dd998579dcda4baec278b
반 지 수 <31958806+cheezzjazz@users.noreply.github.com>
2021-06-22 19:04:23 +0900

    Add files via upload

--- BJS/study_week3/n1806/main.cpp
+++ BJS/study_week3/n1806/main.cpp
@@ -1,27 +1,46 @@
-// 풀이방법 - 수학적 수식으로 z+1을 만드는 최소횟수 a 찾기
-// y+a 와 동시에 x+a 가 됨 주의
-// a 계산식 중 x*z처리시 int범위 초과 주의
+// 풀이방법 - 투포인터
+// sum == s일 때, left를 옮기면 sum < s이므로, right + 1 해야 함
 #include <cstdio>
 #include <iostream>
-#include <cmath>
+#include <vector>
 using namespace std;
+
 int main(int argc, const char * argv[]) {
     freopen("input.txt", "r", stdin);
     int tc; cin>>tc;
     for(int t = 1; t <= tc; ++t) {
-        long long x, y;
-        cin>>x>>y;
-        long long ans = 0;
-        int z = (int)(100*y/x);
-        if(z >= 99)
-            ans = -1;
-        else {
-            long long a = (z*x + x -100*y);
-            long long b = (99.0-z);
-            ans = (a%b == 0)? a / b : a/b +1;
+        int n, s;
+        cin>>n>>s;
+        vector<int> a(n, 0);
+        for(int i = 0; i < n; ++i) {
+            cin>>a[i];
         }
+        int min = 0;
+        
+        int left, right;
+        left = right = 0;
+        int sum = a[0];
+        while(left <= right && right < n) {
+            if(sum > s) {
+                if(min == 0 || min > (right - left+1)) {
+                    min = (right - left)+1;
+                }
+                sum -= a[left];
+                left += 1;
+            } else {
+                if(sum == s) {
+                    if(min == 0 || min > (right - left)) {
+                        min = (right - left)+1;
+                    }
+                }
+                right += 1;
+                if(right < n)
+                    sum += a[right];
+            }
+        }
+        
         cout<<'#'<<t<<' ';
-        cout<<ans<<'\n';
+        cout<<min<<'\n';
     }
     return 0;
 }
