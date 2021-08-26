39791b6a1b524ce5182acee60361536e6cba562c
mintway0341 <44641348+mintway0341@users.noreply.github.com>
2021-08-26 18:51:33 +0900

    Create 11660.cpp

---
+++ mintway0341/dp/11660.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+int n, m, a[1030][1030], b[1030][1030];
+
+int main()
+{
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            scanf("%d", &a[i][j]);
+            b[i][j] = b[i - 1][j] + b[i][j - 1] - b[i - 1][j - 1] + a[i][j];
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        int x1, y1, x2, y2;
+        scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+        printf("%d\n", b[x2][y2] - b[x2][y1 - 1] - b[x1 - 1][y2] + b[x1 - 1][y1 - 1]);
+    }
+
+    return 0;
+}
